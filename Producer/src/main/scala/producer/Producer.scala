8c96ba1d460821c408fd801dc500eb7749e33dba
mind7 <charly.simonian@mind7.fr>
2020-07-16 17:09:44 +0200

    modify build.sbt

---
+++ Producer/src/main/scala/producer/Producer.scala
@@ -0,0 +1,47 @@
+package producer
+
+import java.util.Properties
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.{SparkConf, SparkContext}
+
+
+object Producer extends App{
+
+
+    val props = new Properties()
+    props.put("bootstrap.servers", "localhost:9092")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("group.id", "something")
+
+    val conf = new SparkConf(true)
+      .setAppName("producer")
+      .setMaster("local[*]")
+
+    //Create the KAFKA producer
+    val producer = new KafkaProducer[String, String](props)
+
+
+
+    // Create the SparkContext and the SparkContext
+    val sc = new SparkContext(conf)
+    val ssc = new StreamingContext( sc , Seconds(5))
+
+
+
+    // Read the CSV file with the SparkContext
+    val persRdd = sc.textFile("/home/mind7/Bureau/batch_folder/*.csv")
+    // TODO optimize with spark
+    //implicit val ec = ExecutionContext.global
+    persRdd.collect.foreach( el => {
+
+          val record = new ProducerRecord("test2", el.split(",")(0), el  )
+          producer.send(record)
+
+    })
+
+    producer.close()
+
+}
--- build.sbt
+++ build.sbt
@@ -5,27 +5,27 @@ version := "0.1"
 scalaVersion := "2.11.12"
 
 val sparkVersion = "2.4.3"
-lazy val commonDependencies =
-  libraryDependencies ++= Seq(
-    "org.apache.spark" %% "spark-core" % sparkVersion,
+
+
+lazy val commonDependencies = Seq(
     "org.apache.spark" %% "spark-streaming" % sparkVersion ,
-    "org.apache.spark" %% "spark-sql" % sparkVersion,
     "org.apache.spark" %% "spark-streaming-kafka-0-10" % sparkVersion,
-    "org.apache.spark" %% "spark-sql-kafka-0-10" % sparkVersion,
-    "com.datastax.spark" %% "spark-cassandra-connector" % "3.0-alpha"
+
   )
 
 lazy val Consumer = (project in file("Consumer")).settings(
-  commonDependencies
+  libraryDependencies ++= commonDependencies ++ Seq("com.datastax.spark" %% "spark-cassandra-connector" % "3.0-alpha"),
+  name := "Consumer"
 
 )
 
 lazy val Producer = (project in file("Producer")).settings(
-  commonDependencies
+  libraryDependencies ++= commonDependencies ,
+  name := "Producer"
 
 )
 lazy val Model = (project in file("Model")).settings(
-  commonDependencies
+  libraryDependencies ++= commonDependencies
 
 )
 
